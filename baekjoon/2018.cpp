a52684a420a20f54a3cb77bff26110cfbc8a35a9
donghyun-1 <20dkwak@gmail.com>
2025-05-11 21:03:52 +0900

    2018. 수들의 합 5

---
+++ baekjoon/2018.cpp
@@ -0,0 +1,41 @@
+// https://www.acmicpc.net/problem/2018
+// 수들의 합 5 (실버 5)
+// 2025-05-11
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, ans = 1;
+    cin >> n;
+
+    int start_idx = 1;
+    int end_idx = 1;
+    int sum = 1;
+
+    while (end_idx != n) {
+        if (sum == n) {
+            ans++;
+            end_idx++;
+            sum += end_idx;
+        }
+        else if (sum < n) {
+            end_idx++;
+            sum += end_idx;
+        }
+        else if (sum > n) {
+            sum -= start_idx;
+            start_idx++;
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
