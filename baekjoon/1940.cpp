d568913a22b0c309efb6b6f81fcee769ba3fc794
donghyun-1 <20dkwak@gmail.com>
2025-05-11 21:21:28 +0900

    1940. 주몽

---
+++ baekjoon/1940.cpp
@@ -0,0 +1,43 @@
+// https://www.acmicpc.net/problem/1940
+// 주몽 (실버 4)
+// 2025-05-11
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, m, cnt = 0;
+    cin >> n >> m;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    int i = 0;
+    int j = v.size() - 1;
+
+    while (i < j) {
+        if (v[i] + v[j] == m) {
+            cnt++;
+            j--;
+            i++;
+        }
+        else if (v[i] + v[j] < m) {
+            i++;
+        }
+        else if (v[i] + v[j] > m) {
+            j--;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
